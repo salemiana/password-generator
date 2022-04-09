899ae0b0a0a310f0fb01f49aab0454d174ce99d4
salem z <salem.zerezghi2@gmail.com>
2022-04-09 14:17:28 -0400

    fix

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "BCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var char = ["!", "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var char = ["!", "#", "$", "%", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
 var passwordChar = "";
 var genPassFinal = "";
 var generateBtn = document.querySelector("#generate");
@@ -22,7 +22,7 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var promptUser = window.prompt("What is your password length choice?");
   //conditional statement for 8-128
-  if (promptUser <= 8 || promptUser >=128) {
+  if (promptUser < 8 || promptUser > 128) {
     window.alert("Choose a number between 8-128!");
     return;
   }
@@ -33,33 +33,25 @@ function generatePassword() {
   var passChar = window.confirm("Do you want to include special characters?");
   //for (var i = 0; i >= 8; i++) {}
   var pool = [];
-  if (passNum){
+  if (passNum) {
     pool = pool.concat(num);
   }
-  if (passLower){
+  if (passLower) {
     pool = pool.concat(lowerCase);
   }
-  if (passUpper){
+  if (passUpper) {
     pool = pool.concat(upperCase);
   }
-  if (passChar){
-    pool = pool.concat(passChar);
+  if (passChar) {
+    pool = pool.concat(char);
   }
-for (var i = 0; i < promptUser; i++){
-  var index = Math.floor(Math.random()*pool.length);
-  passwordChar = pool[index];
-  genPassFinal = genPassFinal.concat(passwordChar);
+  for (var i = 0; i < promptUser; i++) {
+    var index = Math.floor(Math.random() * pool.length);
+    passwordChar = pool[index];
+    genPassFinal = genPassFinal.concat(passwordChar);
 
+  }
+  return genPassFinal;
 }
-return genPassFinal;
-}
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
 
 
